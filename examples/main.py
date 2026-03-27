c023c4c704a9efbb938e3a27bd7a954dcb317f03
jtimo <jithutimothy@googlemail.com>
2026-03-27 15:13:44 +0100

    minor update

--- examples/main.py
+++ examples/main.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 import sys,os
-sys.path.append('./../pycmg/')
+sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../pycmg/'))
 from generate_mesostructure import Mesostructure
 from configuration import Configuration
 from visualization import export_data, visualize_sections
 import numpy as np
 
 
-my_configuration = Configuration(vf_max_assembly=0.3, average_shape=[1, 0.5, 0.5])
-my_configuration.load_inclusions(conf_csv='AB8_CMG_full.csv')
+my_configuration = Configuration(vf_max_assembly=0.45, average_shape=[1, 0.5, 0.5])
+my_configuration.load_inclusions(conf_csv=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'AB8_CMG_full.csv'))
 my_mesostructure = Mesostructure(mesostructure_size=[20, 20, 50], resolution=[0.125, 0.125, 0.125])
 my_mesostructure.add_configuration(my_configuration)
 my_virtual_mesostructure = my_mesostructure.assemble_sra()
